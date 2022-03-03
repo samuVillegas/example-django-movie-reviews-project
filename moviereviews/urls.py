6fa21e81f8e98633050ac7568c57aad274711789
samuVillegas <villegassamuel25@gmail.com>
2022-03-03 10:02:00 -0500

    Se realiza pàgina para buscado de peliculas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',movieViews.home)
+    path('',movieViews.home),
+    path('about',movieViews.about)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
